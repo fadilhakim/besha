c816c3d6f50573308c3a0f64f2fea48203a5317f
Aries Dimas Yudhistira <alhusna901@gmail.com>
2017-03-19 11:16:37 +0700

    kunglaw add email2.php

---
+++ application/config/email2.php
@@ -0,0 +1,12 @@
+<?php
+
+	//test@besha-analitika.co.id
+$config['test_email']['protocol']  = 'smtp';
+$config['test_email']['mailtype']  = 'html';
+$config['test_email']['priority']  = '1';
+$config['test_email']['wordwrap']  = FALSE;
+$config['test_email']['smtp_host'] = 'ssl://mail.besha-analitika.co.id';
+$config['test_email']['smtp_port'] = 465;
+$config['test_email']['smtp_user'] = 'test@besha-analitika.co.id';
+$config['test_email']['smtp_pass'] = 'coolmangenius99';
+$config['test_email']['charset']   = 'utf-8';
