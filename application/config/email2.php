59eb7a7e6a02ef9e2bc7092f9f1d4cf1120f6b69
Aries Dimas Yudhistira <alhusna901@gmail.com>
2017-03-30 01:18:03 +0700

    change email config

--- application/config/email2.php
+++ application/config/email2.php
@@ -20,3 +20,14 @@ $config['fadil182_gmail']['smtp_port'] = 465;
 $config['fadil182_gmail']['smtp_user'] = 'fadil.hakim182@gmail.com';
 $config['fadil182_gmail']['smtp_pass'] = 'bismilah1';
 $config['fadil182_gmail']['charset']   = 'iso-8859-1';
+
+
+$config['beshaanalitika99']['protocol']  = 'smtp';
+$config['beshaanalitika99']['mailtype']  = 'html';
+$config['beshaanalitika99']['priority']  = '1';
+$config['beshaanalitika99']['wordwrap']  = FALSE;
+$config['beshaanalitika99']['smtp_host'] = 'ssl://smtp.gmail.com';
+$config['beshaanalitika99']['smtp_port'] = 465;
+$config['beshaanalitika99']['smtp_user'] = 'beshaanalitika99@gmail.com';
+$config['beshaanalitika99']['smtp_pass'] = 'adminbesha';
+$config['beshaanalitika99']['charset']   = 'iso-8859-1';
