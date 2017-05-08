673590b81c7c039aed901143f3ec88d458202668
fadil_hakim <fadil.nylon@gmail.com>
2017-05-08 21:57:10 +0800

    update password besha

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -322,7 +322,7 @@ class cart extends CI_Controller { // Our Cart class extends the Controller clas
 			$config['smtp_host'] = 'ssl://besha-analitika.co.id';
 			$config['smtp_port'] = 465;
 			$config['smtp_user'] = 'info@besha-analitika.co.id';
-			$config['smtp_pass'] = 'info-n23';
+			$config['smtp_pass'] = 'be=$.P!TQ6X*';
 
 			$this->email->initialize($config);
 
@@ -357,7 +357,7 @@ class cart extends CI_Controller { // Our Cart class extends the Controller clas
 		$config['smtp_host'] = 'ssl://besha-analitika.co.id';
 		$config['smtp_port'] = 465;
 		$config['smtp_user'] = 'info@besha-analitika.co.id';
-		$config['smtp_pass'] = 'info-n23';
+		$config['smtp_pass'] = 'be=$.P!TQ6X*';
   
 		$this->email->initialize($config);
   
--- application/views/invoice/invoice-fancy-page-inline.php
+++ application/views/invoice/invoice-fancy-page-inline.php
@@ -41,18 +41,20 @@
         <div id="client" style="padding-left:6px;border-left-width:6px;border-left-style:solid;border-left-color:#0087C3;float:left;width:45%;" >
           <div class="to" style="color:#777777;" >INVOICE TO:</div>
           <h2 class="name" style="font-size:1.4em;font-weight:normal;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;" ><?= $detail_user["contact_person"]?></h2>
-          <div class="address">796 Silver Harbour, TX 79273, US</div>
+          <h2 class="name" style="font-size:1.2em;font-weight:bold;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;" ><?= $detail_user["company_name"]?></h2>
+          <div class="address"><?= $detail_user["address"]?></div>
           <div class="email"><a href="mailto:john@example.com" style="color:#0087C3;text-decoration:none;" ><?=$user_sess["email"]?></a></div>
         </div>
         <div id="invoice" style="float:right;text-align:right;width:45%;" >
           <h1 style="color:#0087C3;font-size:2.4em;line-height:1em;font-weight:normal;margin-top:0;margin-bottom:0;margin-right:;margin-left:10px;" >SURAT PENAWARAN SPAREPART</h1>
-          <div class="date" style="font-size:1.1em;color:#777777;" >Date of Invoice: <?=$create_date?></div>
-          <div class="date" style="font-size:1.1em;color:#777777;" >Due Date: <?=$due_date?></div>
+          <div class="date" style="font-size:1.1em;color:#777777;" >Date of Quotation: <?=$create_date?></div>
+          <!-- <div class="date" style="font-size:1.1em;color:#777777;" >Due Date: <?=$due_date?></div> -->
         </div>
       </div>
       <table border="0" cellspacing="0" cellpadding="0" style="width:100%;border-collapse:collapse;border-spacing:0;margin-bottom:20px;" >
         <thead>
           <tr>
+          <th class="" style="padding-top:20px;padding-bottom:20px;padding-right:20px;padding-left:20px;background-color:#EEEEEE;background-image:none;background-repeat:repeat;background-position:top left;background-attachment:scroll;text-align:center;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#FFFFFF;white-space:nowrap;font-weight:normal;" >NO</th>
             <th class="" style="padding-top:20px;padding-bottom:20px;padding-right:20px;padding-left:20px;background-color:#EEEEEE;background-image:none;background-repeat:repeat;background-position:top left;background-attachment:scroll;text-align:center;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#FFFFFF;white-space:nowrap;font-weight:normal;" >IMAGE</th>
             <th class="desc" style="padding-top:20px;padding-bottom:20px;padding-right:20px;padding-left:20px;background-color:#EEEEEE;background-image:none;background-repeat:repeat;background-position:top left;background-attachment:scroll;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#FFFFFF;white-space:nowrap;font-weight:normal;text-align:left;" >PRODUCT NAME</th>
             <th class="unit" style="padding-top:20px;padding-bottom:20px;padding-right:20px;padding-left:20px;text-align:center;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#FFFFFF;white-space:nowrap;font-weight:normal;background-color:#DDDDDD;background-image:none;background-repeat:repeat;background-position:top left;background-attachment:scroll;" >UNIT PRICE</th>
@@ -61,12 +63,13 @@
           </tr>
         </thead>
         <tbody>
-          <?php
+          <?php $i = 1;
 		  foreach($this->cart->contents() as $items)
 			{
 				$detail_sparepart = $this->model_sparepart->getproductfromIdandCode($items['id'],$items['code'])->row();
 		  ?>
           <tr>
+           <td class="" style="padding-top:20px;padding-bottom:20px;padding-right:20px;padding-left:20px;background-color:#EEEEEE;background-image:none;background-repeat:repeat;background-position:top left;background-attachment:scroll;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#FFFFFF;text-align:right;" ><?php echo $i; $i++;?></td>
             <td class="" style="padding-top:20px;padding-bottom:20px;padding-right:20px;padding-left:20px;background-color:#EEEEEE;background-image:none;background-repeat:repeat;background-position:top left;background-attachment:scroll;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#FFFFFF;text-align:right;" ><img
                     src="<?=check_image_sparepart($items['id'])?>" width="100" height="100" /></td>
             <td class="desc" style="padding-top:20px;padding-bottom:20px;padding-right:20px;padding-left:20px;background-color:#EEEEEE;background-image:none;background-repeat:repeat;background-position:top left;background-attachment:scroll;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#FFFFFF;text-align:left;" ><?=$items["code"]?> / <?=$detail_sparepart->sparepart_name?></td>
@@ -75,7 +78,7 @@
             <td class="total" style="padding-top:20px;padding-bottom:20px;padding-right:20px;padding-left:20px;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#FFFFFF;text-align:right;background-color:#57B223;background-image:none;background-repeat:repeat;background-position:top left;background-attachment:scroll;color:#FFFFFF;font-size:1.2em;" >Rp. <?=number_format($items["subtotal"])?></td>
           </tr>
           <?php
-			}
+			 }
 		  ?>
         </tbody>
         <tfoot>
@@ -93,7 +96,7 @@
           </tr>
           <tr>
             <td colspan="2" style="border-bottom-width:1px;border-bottom-color:#FFFFFF;text-align:right;padding-top:10px;padding-bottom:10px;padding-right:20px;padding-left:20px;background-color:#FFFFFF;background-image:none;background-repeat:repeat;background-position:top left;background-attachment:scroll;border-bottom-style:none;font-size:1.2em;white-space:nowrap;border-top-width:1px;border-top-style:solid;border-top-color:#AAAAAA;" ></td>
-            <td colspan="2" style="border-bottom-width:1px;border-bottom-color:#FFFFFF;text-align:right;padding-top:10px;padding-bottom:10px;padding-right:20px;padding-left:20px;background-color:#FFFFFF;background-image:none;background-repeat:repeat;background-position:top left;background-attachment:scroll;border-bottom-style:none;font-size:1.2em;white-space:nowrap;border-top-width:1px;border-top-style:solid;border-top-color:#AAAAAA;" >SHIPPING </td>
+           <!--  <td colspan="2" style="border-bottom-width:1px;border-bottom-color:#FFFFFF;text-align:right;padding-top:10px;padding-bottom:10px;padding-right:20px;padding-left:20px;background-color:#FFFFFF;background-image:none;background-repeat:repeat;background-position:top left;background-attachment:scroll;border-bottom-style:none;font-size:1.2em;white-space:nowrap;border-top-width:1px;border-top-style:solid;border-top-color:#AAAAAA;" >SHIPPING </td> -->
             <td colspan="2" style="border-bottom-width:1px;border-bottom-color:#FFFFFF;text-align:right;padding-top:10px;padding-bottom:10px;padding-right:20px;padding-left:20px;background-color:#FFFFFF;background-image:none;background-repeat:repeat;background-position:top left;background-attachment:scroll;border-bottom-style:none;font-size:1.2em;white-space:nowrap;border-top-width:1px;border-top-style:solid;border-top-color:#AAAAAA;" >TAX 10%</td>
             <td style="border-bottom-width:1px;border-bottom-color:#FFFFFF;text-align:right;padding-top:10px;padding-bottom:10px;padding-right:20px;padding-left:20px;background-color:#FFFFFF;background-image:none;background-repeat:repeat;background-position:top left;background-attachment:scroll;border-bottom-style:none;font-size:1.2em;white-space:nowrap;border-top-width:1px;border-top-style:solid;border-top-color:#AAAAAA;" >
 
@@ -120,8 +123,10 @@
       </table>
       <div id="thanks" style="font-size:2em;margin-bottom:50px;" >Thank you!</div>
       <div id="notices" style="padding-left:6px;border-left-width:6px;border-left-style:solid;border-left-color:#0087C3;" >
-        <div>NOTICE:</div>
-        <div class="notice" style="font-size:1.2em;" >A finance charge of 1.5% will be made on unpaid balances after 30 days.</div>
+        <div>KETERANGAN :</div>
+        <div class="notice" style="font-size:1.2em;" >1. Harga Nett</div>
+        <div class="notice" style="font-size:1.2em;" >2. Penawaran Berlaku 15 Hari dari  tanggal penawaran</div>
+        <div class="notice" style="font-size:1.2em;" >3. Pembayaran 7 hari setelah barang diterima</div>
       </div>
     </main>
     <footer style="color:#777777;width:100%;height:30px;position:absolute;bottom:0;border-top-width:1px;border-top-style:solid;border-top-color:#AAAAAA;padding-top:8px;padding-bottom:8px;padding-right:0;padding-left:0;text-align:center;" >
